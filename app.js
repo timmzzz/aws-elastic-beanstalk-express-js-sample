32644211a8c1f3762ad71a0159f24ee22b6d9c43
Timm Zimmermann <timmzzz@NewChallanges.fritz.box>
2020-12-15 11:45:12 +0100

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('This will be a new attempt to learn something and making a living'));
+app.get('/', (req, res) => res.send('This will be a new attempt to learn something and making a living // WITH MANUAL REVIEW'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
